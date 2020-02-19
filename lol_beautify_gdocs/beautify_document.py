7ca2c2cdbcb1ab13ee3b0282ec462b2a5e6c7c79
Gary Mialaret <gary.mialaret@gmail.com>
2020-02-19 17:40:48 +0900

    Adding necessary files for PyPI setup

--- lol_beautify_gdocs/beautify_document.py
+++ lol_beautify_gdocs/beautify_document.py
@@ -54,6 +54,7 @@ def beautify_document(document_descriptor: str):
 def create_image_request(match_content, match_position):
     # This splits on < and removes the last character which should be >
     # For example, for '20<Braum>', object_name will be 'Braum' and size will be 20
+    # The replace statement is here to make sure the typographical apostrophe ’ is properly handled
     object_name = match_content.split('<')[1][:-1].replace('’', "'")
     size = int(match_content.split('<')[0])
 
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='lol_beautify_gdocs',
+    packages=['lol_beautify_gdocs'],
+    version='0.1',
+    license='MIT',
+    description='Beautifying Google documents related to League of Legends',
+    author='Gary Mialaret',
+    author_email='gary.mialaret@gmail.com',
+    url='https://github.com/mrtolkien',
+    download_url='https://github.com/mrtolkien/lol_beautify_gdocs/archive/v0.1.tar.gz',
+    keywords=['lol', 'gdocs'],
+    install_requires=[
+        'google-api-python-client',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
+        'static-data',
+        'joblib'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # Again, pick a license
+        'Programming Language :: Python :: 3.8',
+    ],
+)
