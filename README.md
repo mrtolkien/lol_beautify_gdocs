1956f2d261ac1a69896a4635b7c6d70f04868c7d
Gary Mialaret <gary.mialaret@gmail.com>
2020-02-19 14:50:11 +0900

    Adding the code to connect to the google docs API

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # lol_beautify_gdocs
+
+# Installation
+
+- `pip install lol_beautify_gdocs`
+- Go to https://developers.google.com/docs/api/quickstart/python
+- Generate a `credentials.json` file by pressing "Enable the Google Docs API" button
+- Move the file to `~/.config/gsuite_api/credentials.json`
+
---
+++ lol_beautify_gdocs/gdocs_utils.py
@@ -0,0 +1,29 @@
+import joblib
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+def get_service():
+    credentials = None
+    token_location = os.path.join(os.path.join(os.path.expanduser("~"), '.config', 'gsuite_api', 'token.pickle'))
+
+    # The file token.pickle stores the user's access and refresh tokens, and is created
+    # automatically when the authorization flow completes for the first time.
+    if os.path.exists(token_location):
+        credentials = joblib.load(token_location)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                os.path.join(os.path.join(os.path.expanduser("~"), '.config', 'gsuite_api', 'credentials.json')),
+                'https://www.googleapis.com/auth/documents')
+            credentials = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        joblib.dump(credentials, token_location)
+
+    return build('docs', 'v1', credentials=credentials)
